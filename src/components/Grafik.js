169d460cfb49444e4f511dc7bd11358b8c52978f
wantoro33 <wantoro.33@gmail.com>
2020-07-29 15:29:59 +0700

    betulin grafik

--- src/components/Grafik.js
+++ src/components/Grafik.js
@@ -1,28 +1,51 @@
-import React from "react";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import { Typography } from "@material-ui/core";
+import * as React from "react";
+import Paper from "@material-ui/core/Paper";
+import {
+  Chart,
+  BarSeries,
+  Title,
+  ArgumentAxis,
+  ValueAxis,
+} from "@devexpress/dx-react-chart-material-ui";
 
-function Grafik() {
-  return (
-    <Card variant="outlined>">
-      <CardContent>
-        <Typography variant="h1">Grafik</Typography>
-        <Typography variant="h1">Grafik</Typography>
-        <Typography variant="h1">Grafik</Typography>
-        <Typography variant="h1">Grafik</Typography>
-        <Typography variant="h1">Grafik</Typography>
-        <Typography variant="h1">Grafik</Typography>
-        <Typography variant="h1">Grafik</Typography>
-        <Typography variant="h1">Grafik</Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Detail</Button>
-      </CardActions>
-    </Card>
-  );
+import { Animation } from "@devexpress/dx-react-chart";
+
+const data = [
+  { penyakit: "Penyakit A", jumlah: 2.525 },
+  { penyakit: "Penyakit B", jumlah: 3.018 },
+  { penyakit: "Penyakit C", jumlah: 3.682 },
+  { penyakit: "Penyakit D", jumlah: 4.44 },
+  { penyakit: "Penyakit E", jumlah: 5.31 },
+  { penyakit: "Penyakit F", jumlah: 6.127 },
+  { penyakit: "Penyakit G", jumlah: 6.93 },
+  { penyakit: "Penyakit H", jumlah: 5.31 },
+  { penyakit: "Penyakit I", jumlah: 6.127 },
+  { penyakit: "Penyakit J", jumlah: 6.93 },
+];
+
+export default class Grafik extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data,
+    };
+  }
+
+  render() {
+    const { data: chartData } = this.state;
+
+    return (
+      <Paper>
+        <Chart data={chartData}>
+          <ArgumentAxis />
+          <ValueAxis max={7} />
+
+          <BarSeries valueField="jumlah" argumentField="penyakit" />
+          <Title text="10 Penyakit Terbanyak" />
+          <Animation />
+        </Chart>
+      </Paper>
+    );
+  }
 }
-
-export default Grafik;
